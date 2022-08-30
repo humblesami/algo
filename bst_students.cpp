2cbbf63aba425cfeeac256a3e3e2c0cc939012e1
humblesami <samiakram@live.com>
2022-08-30 19:59:06 +0500

    Update bst_students.cpp

--- bst_students.cpp
+++ bst_students.cpp
@@ -33,22 +33,95 @@ int enrolled_students = 0;
 list<Student> students;
 list<Student>::iterator reader;
 
-
-struct StudentComparator
-{
-    // Compare 2 Player objects using name
-    bool operator ()(const Student & st1, const Student & st2)
-    {
-        if(st1.total == st2.total)
-        {
-            return st1.name < st2.name;
-        }
-        return st1.total > st2.total;
-    }
+struct StructNode {
+    Student key;
+    struct StructNode *left, *right;
 };
 
+// Create a node
+struct StructNode *newNode(Student item) {
+    struct StructNode *temp = (struct StructNode *)malloc(sizeof(struct StructNode));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
+}
 
-void add_student(){
+// Inorder Traversal
+void read_bst_inorder(struct StructNode *root) {
+    if (root != NULL) {
+        // Traverse left
+        read_bst_inorder(root->left);
+        // Traverse root
+        root->key.show();
+        // Traverse right
+        read_bst_inorder(root->right);
+    }
+}
+
+// Insert a node
+struct StructNode *insertElementInTree(struct StructNode *node, Student key) {
+    // Return a new node if the tree is empty
+    if (node == NULL) return newNode(key);
+    // Traverse to the right place and insert the node
+    if (key.total > node->key.total)
+    {
+        node->left = insertElementInTree(node->left, key);
+    }
+    else
+    {
+        node->right = insertElementInTree(node->right, key);
+    }
+    return node;
+}
+
+// Find the inorder successor
+struct StructNode *minValueNode(struct StructNode *node) {
+    struct StructNode *current = node;
+    // Find the leftmost leaf
+    while (current && current->left != NULL)
+    {
+        current = current->left;
+    }
+    return current;
+}
+
+// Deleting a node
+struct StructNode *deleteNode(struct StructNode *root, Student key) {
+    // Return if the tree is empty
+    if (root == NULL) return root;
+    // Find the node to be deleted
+    if (key.total < root->key.total)
+    {
+        root->left = deleteNode(root->left, key);
+    }
+    else if (key.total > root->key.total)
+    {
+        root->right = deleteNode(root->right, key);
+    }
+    else {
+        // If the node is with only one child or no child
+        if (root->left == NULL) {
+            struct StructNode *temp = root->right;
+            free(root);
+            return temp;
+        } else if (root->right == NULL) {
+            struct StructNode *temp = root->left;
+            free(root);
+            return temp;
+        }
+        // If the node has two children
+        struct StructNode *temp = minValueNode(root->right);
+        // Place the inorder successor in position of the node to be deleted
+        root->key = temp->key;
+        // Delete the inorder successor
+        root->right = deleteNode(root->right, temp->key);
+    }
+    enrolled_students -= 1;
+    return root;
+}
+
+
+struct StructNode *add_student(struct StructNode *root){
     int total_test_marks = 100;
     int total_matric_marks = 1050;
     int total_inter_marks = 1100;
@@ -64,78 +137,62 @@ void add_student(){
     cout << "\nIntermideate marks => ";
     cin >> st.inter;
     st.total = st.get_total_marks();
-    students.insert(students.begin(), st);
-    students.sort(StudentComparator());
+    root = insertElementInTree(root, st);
     enrolled_students += 1;
+    return root;
 }
 
 
-Student get_student(string enrollment){
-    Student st;
-    int found = 0;
-    for (reader = students.begin(); reader != students.end(); ++reader){
-        if(reader->enrollment == enrollment){
-            st = *reader;
-            found = 1;
-            break;
+void find_student(struct StructNode *root, string enrollment, int cnt) {
+    if (root != NULL) {
+        // Traverse left
+        cnt += 1;
+        find_student(root->left, enrollment, cnt);
+        // Traverse root
+        if(root->key.enrollment == enrollment)
+        {
+            cout<<"Student found in "<<cnt<< " operations, with enrollment => "<<enrollment;
+            root->key.show();
+            return;
         }
+        // Traverse right
+        find_student(root->right, enrollment, cnt);
     }
-    if(found != 1){
-        cout<<"\n\nStudnent NOT found by enrollment => "<<enrollment;
-        return st;
-    }
-    cout<<"\n\nStudnent found by enrollment => "<<enrollment;
-    st.show();
-    return st;
 }
 
-
-void show_top_students(int cnt){
-    Student st;
-    int found = 0;
-    if (cnt > enrolled_students){
-        cnt = enrolled_students;
-    }
-    //students.sort(StudentComparator());
-    int i = 1;
-    for (reader = students.begin(); reader != students.end(); ++reader){
-        if(i > cnt){
-            break;
-        }
-        reader->show();
-        i += 1;
-    }
-    cout<<"\n Stundents shown => "<<enrolled_students;
-}
 
 int main() {
     
     int cnt;
     char option;
     string enrollment;
-    
+    struct StructNode *root = NULL;
     int turns = 1;
     while(option != 'q' && turns < 15){
-        cout <<"\n\n Press 1 to Add student";
-        cout <<"\n Press 2 to Give number to get top students";
-        cout <<"\n Press 3 to Give enrollment to get student";
+        cout <<"\n\n Press 1 to add student";
+        cout <<"\n Press 2 to show list of students";
+        cout <<"\n Press 3 to find a student";
         cout <<"\n Press q to quit";
         cout <<" => ";
         cin >> option;
         switch(option){
             case '1':
-                add_student();
+                root = add_student(root);
                 cout << "\n\n Will add student";
                 break;
             case '2':
-                cout << "\n\n Enter count of top students => ";
-                cin >> cnt;
-                show_top_students(cnt);
+                read_bst_inorder(root);
+                cout<<"\n Number of enrolled/shown students => "<<enrolled_students;
                 break;
             case '3':
-                cout << "\n\n Enter enrollment to find student => ";
-                cin >> enrollment;
-                get_student(enrollment);
+                if(enrolled_students == 0){
+                    cout<<"No students enrolled yet";
+                }
+                else{
+                    cout << "\n\n Enter enrollment to find student => ";
+                    cin >> enrollment;
+                    find_student(root, enrollment, 0);    
+                }
                 break;
             case 'q':
                 cout << "\n\nTerminating Program\n";
@@ -146,5 +203,4 @@ int main() {
     cout << "\n\nProgrm completed!\n";
     return 0;
 }
-
 
